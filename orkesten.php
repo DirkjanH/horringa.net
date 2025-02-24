c686830a22b9abf124570fcf6a6483d2a329e60c
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-02-24 11:07:38 +0100

    d

--- orkesten.php
+++ orkesten.php
@@ -1,44 +1,40 @@
 <!doctype html>
-<html xml:lang="en" lang="en"><!-- InstanceBegin template="/Templates/horringa.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<html xml:lang="nl" lang="nl">
 
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<base href="https://horringa.net/"/>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-	<meta name="keywords" content="dirigent, conductor, chef de chœur, chorleiter, chef d'orchestre, choirmaster, Utrecht, Holland, Netherlands, Nederland, inzingen, vocaal, zang, zangtechniek, klarinet, clarinet, viola, altviool, Bratsche, alto, bariton, baritone, ensemble"/>
-	<meta name="description" content="Musical activities of conductor Dirkjan Horringa"/>
-	<!-- InstanceBeginEditable name="doctitle" -->
-	<title>Dirkjan Horringa, conductor</title>
-	<!-- InstanceEndEditable -->
+	<base href="https://horringa.net/" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="dirigent, conductor, chef de chœur, chorleiter, chef d'orchestre, choirmaster, Utrecht, Holland, Netherlands, Nederland, inzingen, vocaal, zang, zangtechniek, klarinet, clarinet, viola, altviool, Bratsche, alto, bariton, baritone, ensemble" />
+	<meta name="description" content="Musical activities of conductor Dirkjan Horringa" />
+	<title>Dirkjan Horringa, dirigent</title>
 	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="css/horringa.css" rel="stylesheet" type="text/css">
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	<link rel="icon" href="/favicon.ico" type="image/x-icon">
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
 </head>
 
 <body>
-<?php require("navigatie.php"); ?>
+	<?php require("navigatie.php"); ?>
 
-<div class="w3-main w3-padding" style="margin-left: 250px;">
-<span class="w3-opennav w3-hide-large w3-xxlarge" onclick="w3_open()">&#9776;</span>
-<!-- InstanceBeginEditable name="MainText" -->
-	<article style="display: none;">
-		<h2>Het Wachtkamer Orkest</h2>
-		<p><img src="images/Wachtkamerorkest1.jpg" alt="Wachtkamerorkest" class="w3-image" style="margin-right: 15px; margin-bottom: 15px; vertical-align: top;"/><br>
-De oorsprong van Het Wachtkamer Orkest ligt in een jubileumconcert gegeven ter ere van het 35-jarig bestaan van het Frysk Jeugd Orkest in februari 2012. Het orkest bestaat uit enthousiaste amateur strijkers:  viool, altviool, cello en contrabas. Er wordt gerepeteerd in de wachtkamer van Psychologenpraktijk Eewal te Leeuwarden, wat de naam "Wachtkamer Orkest" verklaart. Het orkest is momenteel in ruste.</p>
-	</article>
-	<article>
+	<div class="w3-main w3-padding" style="margin-left: 250px;">
+		<span class="w3-opennav w3-hide-large w3-xxlarge" onclick="w3_open()">&#9776;</span>
+		<article>
+			<h2>La Pellegrina ProjectOrkest</h2>
+			<p><em><img src="images/LP_logo_compact.jpg" alt="LP logo compact" width="300" class="content-left" style="margin-right: 15px; margin-bottom: 15px; float: left;" />La Pellegrina</em> organiseert al sinds 1991 internationale muziekprojecten in Tsjechië. Een groep goedgeschoolde en enthousiaste amateurmusici werkt steeds 10 dagen aan een kamermuziekprogramma en een orkestraal programma, onder leiding van Tsjechische, Nederlandse en internationale docenten, gerenommeerde specialisten op hun vakgebied. Van de deelnemers is ca. 70&nbsp;% Nederlander; de rest komt letterlijk uit de hele wereld. Inmiddels is het traditie om <a href="https://projectorkest.pellegrina.net/" target="_blank">de Tsjechische slotconcerten later in het jaar in Nederland te herhalen</a>. Hiertoe is de stichting <em>La Pellegrina</em> opgericht. Deze stichting heeft tot doel het organisatorisch mogelijk maken van concerten in <em>La-Pellegrina</em>-verband in Nederland. Van tijd tot tijd komen de Nederlandse deelnemers van de zomercursussen ook in Nederland bijeen om hier muziekprojecten te laten plaatsvinden. Die vinden dan plaats onder de naam '<em>La Pellegrina</em> ProjectKoor &amp; -Orkest'.</p>
+		</article>
+		<article>
+			<h2>Mozart op Zaterdag</h2>
+			<p><img src="https://horringa.net/Digiflyers/Mozart.jpg" alt="Mozart" class="w3-image w3-left w3-margin-right" style="max-width: 350px;">In februari 2025 is een nieuw orkest-inititatief in Utrecht gestart: <i>Mozart op Zaterdag</i>! Iedere maand komt een orkest van professionals en goede amateurs bijeen op een zaterdagochtend om een symfonie en/of een (solo)concert van Mozart te repeteren en meteen uit te voeren.</p>
+			<p>Mozart heeft 41 symfonieën geschreven, maar ook 20 pianoconcerten en tal van concerten voor viool, voor blazers en voor diverse combinaties van soloinstrumenten. Al die stukken hebben hun eigen charme én uitdagingen, maar de ervaring leert dat goed voorbereide spelers ze snel heel redelijk kunnen spelen. Daarbij helpt het als er in diverse groepen een professional (als aanvoerder) meespeelt. Vakstudenten en professionals worden aangetrokken om een van de concerten te spelen. Zo ontstaat iedere keer een programma van 30 à 45 minuten muziek. De eerste keer vond plaats op zaterdag 22 februari in het Stadsklooster Utrecht, met Mozarts 'kleine g-moll' symfonie nr. 25 KV 183 als onderwerp en een orkest van zo'n 30 strijkers en blazers als deelnemers, onder leiding van dirigent en initiatiefnemer Dirkjan Horringa. Een 50-tal mensen kwam luisteren en was enthousiast.</p>
+			<p>Het is de bedoeling hier een maandelijkse traditie van te maken op de vierde zaterdag van de maand. Dus de volgende keren zijn op 22 maart, 26 april en 24 mei, en daarna weer op 27 september, 25 oktober en 22 november. <a href="moz.horringa.net" target="_blank">Meer informatie over komende afleveringen vind je op deze website</a>. Het is mogelijk je op te geven om mee te spelen. <a href="https://forms.gle/49YfM2dSn8AYpzfE8" target="_blank">In dit formulier kun je aangeven of je wilt meedoen en wanneer je beschikbaar bent</a>.</p>
+		</article>
 
-		<h2>La Pellegrina ProjectOrkest</h2>
-		<p><em><img src="images/LP_logo_compact.jpg" alt="LP logo compact" width="300" class="content-left" style="margin-right: 15px; margin-bottom: 15px; float: left;" />La Pellegrina</em> organiseert al sinds 1991 internationale muziekprojecten in Tsjechië. Een groep goedgeschoolde en enthousiaste amateurmusici werkt steeds 10 dagen aan een kamermuziekprogramma en een orkestraal programma, onder leiding van Tsjechische, Nederlandse en internationale docenten, gerenommeerde specialisten op hun vakgebied. Van de deelnemers is ca. 70&nbsp;% Nederlander; de rest komt letterlijk uit de hele wereld. Inmiddels is het traditie om <a href="https://projectorkest.pellegrina.net/" target="_blank">de Tsjechische slotconcerten later in het jaar in Nederland te herhalen</a>. Hiertoe is de stichting <em>La Pellegrina</em> opgericht. Deze stichting heeft tot doel het organisatorisch mogelijk maken van concerten in <em>La-Pellegrina</em>-verband in Nederland. Van tijd tot tijd komen de Nederlandse deelnemers van de zomercursussen ook in Nederland bijeen om hier muziekprojecten te laten plaatsvinden. Die vinden dan plaats onder de naam '<em>La Pellegrina</em> ProjectKoor &amp; -Orkest'.</p>
-	</article>
-<!-- InstanceEndEditable -->
+		<h4><a href="javascript: history.go(-1)">Terug</a></h4>
+	</div>
 
-<h4><a href="javascript: history.go(-1)">Terug</a></h4>
-</div>
-	
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+
+</html>
