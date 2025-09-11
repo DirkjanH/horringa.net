72fb3d8497d68cffdd7cd6fbdc2566d225b0fe9f
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-11 20:22:27 +0200

    FBVGf

--- pas.php
+++ pas.php
@@ -1,8 +1,7 @@
 <?PHP
 require_once("./include/membersite_config.php");
 
-if(!$fgmembersite->CheckLogin())
-{
+if (!$fgmembersite->CheckLogin()) {
     $fgmembersite->RedirectToURL("login.php");
     exit;
 }
@@ -10,35 +9,37 @@ if(!$fgmembersite->CheckLogin())
 
 <!doctype html>
 <html xml:lang="en" lang="en">
+
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<base href="https://horringa.net/"/>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-	<meta name="keywords" content="dirigent, conductor, chef de chœur, chorleiter, chef d'orchestre, choirmaster, Utrecht, Holland, Netherlands, Nederland, inzingen, vocaal, zang, zangtechniek, klarinet, clarinet, viola, altviool, Bratsche, alto, bariton, baritone, ensemble"/>
-	<meta name="description" content="Musical activities of conductor Dirkjan Horringa"/>
-<title>Dirkjan Horringa, conductor</title>
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link href="css/horringa.css" rel="stylesheet" type="text/css">
-	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="https://horringa.net/" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="dirigent, conductor, chef de chœur, chorleiter, chef d'orchestre, choirmaster, Utrecht, Holland, Netherlands, Nederland, inzingen, vocaal, zang, zangtechniek, klarinet, clarinet, viola, altviool, Bratsche, alto, bariton, baritone, ensemble" />
+    <meta name="description" content="Musical activities of conductor Dirkjan Horringa" />
+    <title>Dirkjan Horringa, conductor</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/horringa.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
-<div class="w3-main w3-padding" style="margin-left: 250px;">
-<span class="w3-opennav w3-hide-large w3-xxlarge" onclick="w3_open()">&#9776;</span>
-<div class="w3-content w3-panel w3-card-8 w3-center">
-   <p>Passport Dirkjan Horringa:</p>
-   <p><img src="images/pas_DjH.jpg" alt="paspoort DjH" width="700" height="1010"></p>
-   <p>Passport Annemieke Zwanenburg: </p>
-   <p><img src="images/pas_AZ.jpg" alt="paspoort AZ" width="700" height="1024"></p>
-   <p>Driving license Dirkjan Horringa #1:</p>
-   <p><img src="images/rijbewijs_1.jpg" width="703" height="439" alt="rijbewijs 1"></p>
-   <p>Driving license Dirkjan Horringa #1:</p>
-   <p><img src="images/rijbewijs_2.jpg" width="703" height="458" alt=""></p>
-</div>
-<h4><a href="javascript: history.go(-1)">Terug</a></h4>
-</div>
-	
+    <div class="w3-main w3-padding" style="margin-left: 250px;">
+        <span class="w3-opennav w3-hide-large w3-xxlarge" onclick="w3_open()">&#9776;</span>
+        <div class="w3-content w3-panel w3-card-8 w3-center">
+            <p>Passport Dirkjan Horringa:</p>
+            <p><img src="images/pas_DjH.jpg" alt="paspoort DjH" width="700" height="1010"></p>
+            <p>Passport Annemieke Zwanenburg: </p>
+            <p><img src="images/pas_AZ.jpg" alt="paspoort AZ" width="700" height="1024"></p>
+            <p>Driving license Dirkjan Horringa #1:</p>
+            <p><img src="images/rijbewijs_1.jpg" width="703" height="439" alt="rijbewijs 1"></p>
+            <p>Driving license Dirkjan Horringa #1:</p>
+            <p><img src="images/rijbewijs_2.jpg" width="703" height="458" alt=""></p>
+        </div>
+        <h4><a href="javascript: history.go(-1)">Terug</a></h4>
+    </div>
+
 </body>
+
 </html>
